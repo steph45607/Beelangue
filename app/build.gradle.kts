ec49814652bdecb245146b0e4618224b77fdbd19
steph45607 <staniswinata.stephanie@gmail.com>
2024-05-11 18:43:14 +0700

    connect firestore datbase, getLanguage func

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,25 +39,29 @@ android {
 }
 
 dependencies {
-    implementation("com.google.firebase:firebase-database:21.0.0")
     val cameraxVersion = "1.3.3";
     val fragment_version = "1.7.0"
-    // Java language implementation
     implementation("androidx.fragment:fragment:$fragment_version")
-    // Kotlin
-    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics:22.0.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-ml-model-interpreter:22.0.4")
+
     api("com.google.android.material:material:1.11.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
     implementation("com.google.cloud:google-cloud-translate:2.42.0")
-    implementation("com.google.firebase:firebase-ml-model-interpreter:22.0.4")
     implementation("com.google.mlkit:object-detection:17.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/beelangue/MainActivity.java
+++ app/src/main/java/com/example/beelangue/MainActivity.java
@@ -2,20 +2,49 @@ package com.example.beelangue;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
     Button profileBtn;
     ImageButton flagBtn;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void getLanguages(){
+        db.collection("language")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot doc: task.getResult()){
+                                Log.d("test", doc.getId() + " =>" + doc.getData());
+                            }
+                        }
+                        else{
+                            Log.w("test", "Error getting documents", task.getException());
+                        }
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+        getLanguages();
 
         profileBtn = findViewById(R.id.profile);
         profileBtn.setOnClickListener(
