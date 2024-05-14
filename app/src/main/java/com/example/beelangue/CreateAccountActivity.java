f21ce54d00ed36da89a6c8fa4dc564383898aa98
ArishMada <arishmadataly@gmail.com>
2024-05-15 00:23:51 +0700

    Update CreateAccountActivity.java
    save username in real time db

--- app/src/main/java/com/example/beelangue/CreateAccountActivity.java
+++ app/src/main/java/com/example/beelangue/CreateAccountActivity.java
@@ -3,6 +3,7 @@ package com.example.beelangue;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -24,6 +29,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     Button loginButton, createButton ;
     EditText emailText, passwordText, usernameText;
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
 
     @Override
@@ -32,6 +38,8 @@ public class CreateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.create_account);
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance("https://beelangue-d5b83-default-rtdb.asia-southeast1.firebasedatabase.app").getReference();
 
         loginButton = findViewById(R.id.loginBtn);
         loginButton.setOnClickListener(
@@ -100,6 +108,9 @@ public class CreateAccountActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Registration successful
+                            String userId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+                            saveUsername(userId, username);
+
                             Toast.makeText(getApplicationContext(),
                                             "Registration successful!",
                                             Toast.LENGTH_LONG)
@@ -107,6 +118,8 @@ public class CreateAccountActivity extends AppCompatActivity {
 
                             Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
                             startActivity(intent);
+
+
                         } else {
                             // Check if email is already in use
                             String errorMessage = Objects.requireNonNull(task.getException()).getMessage();
@@ -123,6 +136,28 @@ public class CreateAccountActivity extends AppCompatActivity {
                                                 Toast.LENGTH_LONG)
                                         .show();
                             }
+                        }
+                    }
+                });
+    }
+
+    private void saveUsername(String userId, String username) {
+        Log.d("koesmanto", "Attempting to save username for user: " + userId);
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("username", username);
+
+        mDatabase.child("users").child(userId).setValue(userMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("koesmanto", "Username saved successfully for UID: " + userId);
+                        } else {
+                            // Failed
+                            Toast.makeText(getApplicationContext(),
+                                            "Failed to save username",
+                                            Toast.LENGTH_LONG)
+                                    .show();
                         }
                     }
                 });
