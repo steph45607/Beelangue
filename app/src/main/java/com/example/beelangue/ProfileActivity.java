50def5ecf38b9eaf9685224613e79179d4614dea
ArishMada <arishmadataly@gmail.com>
2024-05-18 17:19:43 +0700

    change username

--- app/src/main/java/com/example/beelangue/ProfileActivity.java
+++ app/src/main/java/com/example/beelangue/ProfileActivity.java
@@ -5,8 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +29,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
     ImageButton backBtn;
+    Button changeUsernameBtn;
     Button logoutBtn;
     Button deleteAccountBtn;
     FirebaseAuth mAuth;
@@ -56,6 +60,14 @@ public class ProfileActivity extends AppCompatActivity {
 
         usernameTextView = findViewById(R.id.textView12);
         setUsername();
+
+        changeUsernameBtn = findViewById(R.id.changeUsername);
+        changeUsernameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeUsernameDialog();
+            }
+        });
 
         deleteAccountBtn = findViewById(R.id.deleteAccount);
         deleteAccountBtn.setOnClickListener(new View.OnClickListener() {
@@ -105,6 +117,55 @@ public class ProfileActivity extends AppCompatActivity {
             });
         }
     }
+
+    private void ChangeUsernameDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change Username");
+
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.changeusername, (ViewGroup) findViewById(android.R.id.content), false);
+        final EditText input = viewInflated.findViewById(R.id.editTextUsername);
+        builder.setView(viewInflated);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                String newUsername = input.getText().toString().trim();
+                if (!newUsername.isEmpty()) {
+                    updateUsername(newUsername);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Username cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void updateUsername(final String newUsername) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            mDatabase.child("users").child(userId).child("username").setValue(newUsername).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        usernameTextView.setText(newUsername);
+                        Toast.makeText(getApplicationContext(), "Username updated", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Failed to update username", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
 
     // send alert for deleting
     private void deleteAccountPrompt() {
